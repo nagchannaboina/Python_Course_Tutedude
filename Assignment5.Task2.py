8ae16648c3e0af332edbf001c57ead6b0ba96cd5
nagchannaboina <nchannaboina@gmail.com>
2025-04-21 17:00:22 -0400

    Add files via upload
    Assignment 5 updated

---
+++ Assignment5.Task2.py
@@ -0,0 +1,7 @@
+num_list = list(range(1,11))
+
+print('Original List: ',num_list)
+first_five = num_list[:5]
+print('Extracted First Five elements:',first_five)
+first_five.reverse()
+print('Reversed Extracted elements: ', first_five)
