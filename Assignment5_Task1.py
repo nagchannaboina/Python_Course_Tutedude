3b9652bf0ee1d624094ca6add598dcde7d0f6ccf
nagchannaboina <nchannaboina@gmail.com>
2025-04-24 14:41:19 -0400

    Update Assignment5_Task1.py

--- Assignment5_Task1.py
+++ Assignment5_Task1.py
@@ -3,12 +3,9 @@
 # A dictionary named student_marks which has student name and their marks.
 student_marks = {'Alice': 85, 'Jack': 66, 'Mike': 96, 'Sam':90, 'James': 78, 'Altman': 25}
 
-# i want to make the search with out case sensitivity
-student_marks_lower = {key.lower(): value for key, value in student_marks.items()}
-
 student_name = input('Enter the student\'s name : ')
+if student_name in student_marks:
 
-if student_name in student_marks_lower:
-    print('{}\'s marks: {}'.format(student_name.capitalize(), student_marks_lower[student_name]))
+    print('{}\'s marks: {}'.format(student_name, student_marks[student_name]))
 else:
     print('Student not found in the dictionary, please update it')
